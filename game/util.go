e13f0b0a44d8a17a013dbc6f58a032dbf3ce9b16
Yoremi <yoremi-trad@proton.me>
2026-02-17 19:23:13 +0100

    Add files via upload

---
+++ game/util.go
@@ -0,0 +1,27 @@
+package game
+
+import (
+	"os"
+	"strings"
+)
+
+func ScriptCanLoad(name string) bool {
+	for _, n := range ScriptBlackList {
+		if strings.ToUpper(name) == strings.ToUpper(n) {
+			return false
+		}
+	}
+	return true
+}
+
+func IsExistDir(filepath string) (exist bool, dir bool) {
+	s, err := os.Stat(filepath)
+	exist = true
+	if err != nil {
+		exist = os.IsExist(err)
+	}
+	if exist {
+		dir = s.IsDir()
+	}
+	return
+}
